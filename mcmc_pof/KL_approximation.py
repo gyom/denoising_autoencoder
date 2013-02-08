modified a small bug in creation of dae_untied_weights and in the process of changing the KL measurements

mcmc_pof/KL_approximation.py
@@ -48,7 +48,7 @@ def pdf(true_samples, model_samples, stddev):
     true_samples of shape (N,d)
     model_samples of shape (M,d)
 
-    Returns an array of (N,).
+    Returns an array of (N,M).
     This function should be seen as a vectorial function.
     """
     assert true_samples.shape[1] == model_samples.shape[1]
@@ -59,10 +59,14 @@ def pdf(true_samples, model_samples, stddev):
     # Might as well to through the list iterating.
     results = []
     for n in np.arange(N):
-        results.append( 1 / np.sqrt(2*np.pi) / (stddev**d) * np.exp( - 0.5 * ((true_samples[n,:] - model_samples)**2).sum(axis=1) / stddev**2 ).sum() )
+        results.append( 1 / np.sqrt(2*np.pi) / (stddev**d) * np.exp( - 0.5 * ((true_samples[n,:] - model_samples)**2).sum(axis=1) / stddev**2 ) )
 
-    return np.concat(results)
+    return np.vstack(results)
     #return 1 / np.sqrt(2*np.pi) / (stddev**d) * np.exp( - 0.5 * cross_differences(true_samples, model_samples) / stddev**2 ).mean(axis=1)
+
+
+def cross_entropy(true_samples, model_samples, stddev):
+    return - np.log( pdf(true_samples, model_samples, stddev).mean(axis=1) ).mean()
 
 
 # helper function
@@ -73,12 +77,12 @@ def fit_stddev(true_samples, model_samples, initial_stddev):
     """
 
     current_stddev = initial_stddev
-    current_loglik = np.log(pdf(true_samples, model_samples, current_stddev)).mean()
+    current_loglik = - cross_entropy(true_samples, model_samples, current_stddev)
 
     # Python doesn't have a do-while control flow structure.
     # We have to fake one.
     proposed_stddev = current_stddev / 2.0
-    proposed_loglik = np.log(pdf(true_samples, model_samples, proposed_stddev)).mean()
+    proposed_loglik = - cross_entropy(true_samples, model_samples, proposed_stddev)
 
     while current_loglik < proposed_loglik:
         print "We just accepted the stddev %f transitioning from loglikelihood %f to %f." % (proposed_stddev, current_loglik, proposed_loglik)
@@ -87,16 +91,33 @@ def fit_stddev(true_samples, model_samples, initial_stddev):
         current_loglik = proposed_loglik
         # propose a new one
         proposed_stddev = current_stddev / 2.0
-        proposed_loglik = np.log(pdf(true_samples, model_samples, proposed_stddev)).mean()
+        proposed_loglik = - cross_entropy(true_samples, model_samples, proposed_stddev)
 
     return current_stddev
 
 
 # main function provided by this module
-def KL(true_samples, model_samples, true_samples_Ntrain, true_samples_Ntest, stddev = None):
+def KL(true_samples, model_samples, true_samples_Ntrain = None, true_samples_Ntest = None, stddev = None):
 
     assert type(true_samples) == np.ndarray
     assert type(model_samples) == np.ndarray
+
+    # we can accept proportions between [0.0, 1.0] as arguments
+    if true_samples_Ntrain > 0.0 and true_samples_Ntrain < 1.0:
+        true_samples_Ntrain = int(true_samples.shape[0] * true_samples_Ntrain)
+    if true_samples_Ntest > 0.0 and true_samples_Ntest < 1.0:
+        true_samples_Ntest = int(true_samples.shape[0] * true_samples_Ntest)
+
+    # If one of the quantities is missing, find the appropriate value.
+    # If both are missing, use half for each.
+    if true_samples_Ntrain == None and true_samples_Ntest == None:
+        true_samples_Ntrain == int(true_samples.shape[0]/2)
+        true_samples_Ntest == true_samples.shape[0] - true_samples_Ntrain
+    elif true_samples_Ntrain == None:
+        true_samples_Ntrain == true_samples.shape[0] - true_samples_Ntest
+    elif true_samples_Ntest == None:
+        true_samples_Ntest == true_samples.shape[0] - true_samples_Ntrain
+
 
     assert true_samples_Ntrain + true_samples_Ntest <= true_samples.shape[0]
     ind = np.arange(0,true_samples.shape[0])
@@ -112,11 +133,86 @@ def KL(true_samples, model_samples, true_samples_Ntrain, true_samples_Ntest, std
     # but we're using so many sample points that it should
     # be safe (given that we have only one parameter).
 
-    KL_divergence = (   np.log(pdf(true_samples[ind[0:true_samples_Ntest],:], true_samples[ind[0:true_samples_Ntest],:], stddev)).mean()
-                      - np.log(pdf(true_samples[ind[0:true_samples_Ntest],:], model_samples,                             stddev)).mean() )
+    #KL_divergence = (   np.log(pdf(true_samples[ind[0:true_samples_Ntest],:], true_samples[ind[0:true_samples_Ntest],:], stddev)).mean()
+    #                  - np.log(pdf(true_samples[ind[0:true_samples_Ntest],:], model_samples,                             stddev)).mean() )
+
+    KL_divergence = ( - cross_entropy( true_samples[ind[0:true_samples_Ntest],:], true_samples[ind[0:true_samples_Ntest],:], stddev)
+                      + cross_entropy( true_samples[ind[0:true_samples_Ntest],:], model_samples,                             stddev) )
+
 
     return KL_divergence
 
+### end of this module ###
 
 
-###
+### a basic main function to be used ###
+
+
+def usage():
+    print "-- usage example --"
+
+
+def main():
+    """
+    This main function is not expected to be used often,
+    but it's pretty much a wrapper around the "KL" function
+    above.
+
+    It takes two arguments, which represent file names containing
+    samples from the true distribution and from the model
+    distribution.
+
+    The notation used will be that we are interested in
+    computing KL(p||q) = \int p log (p/q)
+    """
+
+    import os, sys, getopt
+    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hv", ["help", "p_samples_pkl=", "q_samples_pkl=", "stddev=", "p_Ntrain=", "p_Ntest="])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+
+    p_samples_pkl = None
+    q_samples_pkl = None
+    p_Ntrain = None
+    q_Ntrain = None
+    stddev = None
+
+    verbose = False
+    for o, a in opts:
+        if o == "-v":
+            # unused
+            verbose = True
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("--p_samples_pkl"):
+            p_samples_pkl = a
+            assert os.path.exists(p_samples_pkl)
+        elif o in ("--q_samples_pkl"):
+            q_samples_pkl = a
+            assert os.path.exists(q_samples_pkl)
+        elif o in ("--p_Ntrain"):
+            p_Ntrain = int(a)
+        elif o in ("--p_Ntest"):
+            p_Ntest = int(a)
+        else:
+            assert False, "unhandled option"
+
+    assert p_samples_pkl
+    assert q_samples_pkl
+
+    import cPickle
+    p_samples = cPickle.load(open(p_samples_pkl))
+    q_samples = cPickle.load(open(q_samples_pkl))
+
+    KL_value = KL(p_samples, q_samples, p_Ntrain, p_Ntest, stddev)
+    print KL_value
+
+
+if __name__ == "__main__":
+    main()

mcmc_pof/script_2.py
@@ -95,15 +95,26 @@ def main():
             c=c, b=b,
             s=s, act_func=['sigmoid', 'id'])
         reference_langevin_lambda = 0.4**2
+        #reference_langevin_lambda = 1.0
         # overwrite whichever langevin lambda was given as argument
         #sampling_options["langevin_lambda"] = reference_langevin_lambda
         r = lambda x: the_dae.encode_decode(x.reshape((-1,n_inputs))).reshape((n_inputs,))
         grad_E = lambda x: - (r(x) - x) / reference_langevin_lambda
         sampling_options["grad_E"] = grad_E
 
-        previous_samples = cPickle.load(open('/u/alaingui/Documents/tmp/Salah_DAE_2013_02_06_tmp/metropolis_hastings_langevin_grad_E/1360201931/samples.pkl'))
-        sampling_options["x0"] = previous_samples[-1,:]
+        # METHOD 1
+        mnist_dataset = cPickle.load(open('/data/lisa/data/mnist/mnist.pkl', 'rb'))
+        mnist_train = mnist_dataset[0]
+        mnist_train_data, mnist_train_labels = mnist_train
+        ind = np.random.randint(0, mnist_train_data.shape[0]-1)
+        sampling_options["x0"] = mnist_train_data[ind,:]
+        print "Starting the simulation from MNIST digit %d" % mnist_train_labels[ind]
+        # METHOD 2
+        #previous_samples = cPickle.load(open('/u/alaingui/Documents/tmp/Salah_DAE_2013_02_06_tmp/metropolis_hastings_langevin_grad_E/1360201931/samples.pkl'))
+        #sampling_options["x0"] = previous_samples[-1,:]
+        # METHOD 3
         #sampling_options["x0"] = np.random.uniform(low=0.0, high=1.0,size=(n_inputs,))
+
         # these are the only valid sampling methods because they rely only on grad_E
         assert sampling_options["mcmc_method"] in ["langevin", "metropolis_hastings_grad_E", "metropolis_hastings_langevin_grad_E"]
         